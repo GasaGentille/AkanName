12c0594cfe3500ac12197a2a091559b2a685c088
Gentille <gisagentille55@gmail.com>
2019-07-21 19:05:22 +0200

    updating the js file

--- akanName.js
+++ akanName.js
@@ -1,18 +1,6 @@
 function formReset(){
     document.getElementById("form_data").reset();
 }
-// function formValidation(){
-//     var birthDate=document.getElementById("birthdate").value;
-//     var gender=document.getElementById("gender").value;
-    
-//     if(gender != "Male" || gender != "Female"){
-//         alert("Enter valid birthday and choose your gender");
-//         document.getElementById("display").innerHTML="";
-        
-//     }
-//     return;
-// }
-
 
 
 function akanNames(){
@@ -24,7 +12,6 @@ var birthDate=document.getElementById("birthdate").value;
 
 var centry=(year-1)/100+1; 
 var dayOfTheWeek = ( centry/4 -2*centry-1 + 5*year/4  + 26*(month+1)/10 + day ) % 7;
-// console.log(birthDate);
     
 document.getElementById("display").innerHTML=Math.round(dayOfTheWeek);
 var maleNames=["Kwasi"," Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
@@ -32,10 +19,8 @@ var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 var gender=document.getElementById("gender").value;
 var daysOfWeek=["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Suturday"];
 var dOfWeek = Math.round(dayOfTheWeek)
-// formValidation();
 if(!isNaN(dOfWeek)){
     if(gender=="Male"){
-    // console.log(maleNames[Math.round(dayOfTheWeek)]);
         
         document.getElementById("display").innerHTML="You have born on "+daysOfWeek[dOfWeek]+ " and Your akana name is " + maleNames[Math.round(dayOfTheWeek)];
     }
